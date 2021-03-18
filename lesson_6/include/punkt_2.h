fa562c6eca5bbcf9f023453b322f1517e0128d3c
Aleksey Kaurov <titan154@ya.ru>
2021-03-18 08:41:58 +0400

    punkt 2 -done

--- lesson_6/include/punkt_2.h
+++ lesson_6/include/punkt_2.h
@@ -1,1 +1,12 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
+template <class _Elem, class _Traits>
+basic_ostream<_Elem, _Traits>& __CLRCALL_OR_CDECL endll(
+    basic_ostream<_Elem, _Traits>& _Ostr){
+    _Ostr.put(_Ostr.widen('\n'));
+    _Ostr.put(_Ostr.widen('\n'));
+    _Ostr.flush();
+    return _Ostr;
+}
--- lesson_6/lesson_6.cpp
+++ lesson_6/lesson_6.cpp
@@ -1,10 +1,14 @@
 ﻿#include "include/lesson_6.h"
 
+
 int main()
 {
     std::cout << "lesson_6\n";
 
     int value = getIn();
+    cout << value << endl;
+    cout << value << endll;
+    cout << value << endl;
     cout << value << endl;
 }
 
